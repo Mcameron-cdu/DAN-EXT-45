a68a3b65b3ca949b463263e08665de43d52e3b23
Alex Young <s389603@students.cdu.edu.au>
2025-09-03 21:17:47 +1000

    Creating lists which store all temps for each season, then storing the averages to average_temp.txt

--- code/q2_temperature.py
+++ code/q2_temperature.py
@@ -7,13 +7,19 @@ files = glob.glob("assignment_files/temperatures/*.csv")
 # Dictionary: station - list of all temps
 station_data = {}
 
+#Creating a list for all the temperature values for each season to belong
+summer = []
+autumn = []
+winter = []
+spring = []
+
 for fname in files:
     with open(fname, "r") as f:
         reader = csv.reader(f)
         next(reader)  # skip header row
         for row in reader:
             station = row[0].strip()
-            temps = [t for t in row[5:] if t]  # monthly temps, ignore blanks
+            temps = [t for t in row[4:] if t]  # monthly temps, ignore blanks
             temps = [float(t) for t in temps if t.lower() != "nan"]  # skip NaN
 
             if station not in station_data:
@@ -21,6 +27,37 @@ for fname in files:
 
             station_data[station].extend(temps)
 
+            summer.extend([row[4], row[5], row[15]])  #looping through so that the values in the index positions of the months corresponding to each season are added
+            autumn.extend([row[6], row[7], row[8]])
+            winter.extend([row[9], row[10], row[11]])
+            spring.extend([row[12], row[13], row[14]])
+      
+# Question 1: Find the average temperature for all seasons across all years
+
+season_temps = [summer, autumn, winter, spring]      #storing all the temperatures in a nested list
+season_averages = []       #creating a list to store values for each season's average
+for i in range(4):
+    sum = 0
+    for j in range(6270):
+        sum = sum + float(season_temps[i][j])
+        average = sum / 6270
+    season_averages.append(average)
+print(season_averages)
+summer_average = season_averages[0]
+autumn_average = season_averages[1]
+winter_average = season_averages[2]
+spring_average = season_averages[3]
+
+#Save results to file
+with open("assignment_files/average_temp.txt", "w") as out:
+    out.write("Summer:" + summer_average + "\n")
+print("Saved results to assignment_files/average_temp.txt")
+
+
+
+
+
+"""
 # 2. Calculate range (max - min) for each station
 station_ranges = {}
 for station, vals in station_data.items():
@@ -46,6 +83,4 @@ with open("assignment_files/largest_temp_range_station.txt", "w") as out:
         out.write(f"{st}: Range {rg:.1f}°C (Max: {mx:.1f}°C, Min: {mn:.1f}°C)\n")
 
 print("Saved results to assignment_files/largest_temp_range_station.txt")
-            
-            
-            
+"""
